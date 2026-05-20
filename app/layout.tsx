efb518e0171ce25137efe5d070441b4cb780ded2
Supraja <suprajapalagiri96@gmail.com>
2026-05-20 11:04:13 +0530

    meta pixel code added in layout page

--- app/layout.tsx
+++ app/layout.tsx
@@ -63,6 +63,15 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${inter.variable} ${manrope.variable}`}>
       <body className="font-sans">
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=27233108489626598&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
         {/* ================= Google Analytics ================= */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-WF2VTZ5Z21"
@@ -75,6 +84,22 @@ export default function RootLayout({
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
     gtag('config', 'G-WF2VTZ5Z21');
+  `}
+        </Script>
+        {/* ================= Meta Pixel ================= */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+
+    fbq('init', '27233108489626598');
+    fbq('track', 'PageView');
   `}
         </Script>
 
