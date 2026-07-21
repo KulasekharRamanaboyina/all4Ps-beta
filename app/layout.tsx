524823c79035e4e9d480c794d472a8cf6e4a2091
Supraja <suprajapalagiri96@gmail.com>
2026-07-21 14:33:33 +0530

    GTM code add

--- app/layout.tsx
+++ app/layout.tsx
@@ -64,6 +64,27 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${inter.variable} ${manrope.variable}`}>
       <body className="font-sans relative bg-[#05030A] text-white overflow-x-hidden">
+        <Script id="gtm" strategy="afterInteractive">
+          {`
+(function(w,d,s,l,i){w[l]=w[l]||[];
+w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});
+var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+j.async=true;
+j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MQ34XNL7');
+`}
+        </Script>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MQ34XNL7"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <ParticleBackground />
         <noscript>
           <img
